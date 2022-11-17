e510a66ec7702e5ff649cfc7c25e25e1d3bf3cfe
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-17 18:39:33 +0530

    Added CabInvoiceGeneratorUC4

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -27,10 +27,25 @@ public class CabInvoiceTest {
     @Test
     public void givenMultipleRidesShould_ReturnInvoice(){
         CabInvoice cabinvoice = new CabInvoice();
-        Invoice invoice = new Invoice();
         Ride[] rides ={new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
         Invoice actualInvoice = cabinvoice.getInvoiceOfRides(rides);
         Invoice expectedInvoice = new Invoice(3,273,91);
         Assertions.assertEquals(expectedInvoice,actualInvoice);
+    }
+    @Test
+    public void givenUserId_OfMultipleRidesShould_ReturnInvoice(){
+        CabInvoice cabinvoice = new CabInvoice();
+        Ride[] rides1 = {new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
+        Ride[] rides2 = {new Ride(12.5,4.8),new Ride(34.4,9.7)};
+        RideRepository repository = new RideRepository();
+        repository.addRides("user101",rides1);
+        repository.addRides("user102",rides2);
+        Invoice actualInvoice1 = repository.getInvoiceFromUserId("user102");
+        Invoice actualInvoice2 = repository.getInvoiceFromUserId("user101");
+        Invoice expectedInvoice1 = new Invoice(2,483.5,241.75);
+        Invoice expectedInvoice2 = new Invoice(3,273,91);
+        Assertions.assertEquals(expectedInvoice1,actualInvoice1);
+        Assertions.assertEquals(expectedInvoice2,actualInvoice2);
+
     }
 }
