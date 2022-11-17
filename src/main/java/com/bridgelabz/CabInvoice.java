7832ec0ab9b884f7e67cb0e001609eaeb55cb865
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-11-17 14:27:24 +0530

    Added CabInvoiceGeneratorUC2

--- src/main/java/com/bridgelabz/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoice.java
@@ -3,11 +3,12 @@ package com.bridgelabz;
 public class CabInvoice {
     static final int FARE_PER_KM = 10;
     static final int FARE_PER_MINUTE = 1;
+    static final int MINIMUM_FARE = 5;
     public double getTotalFare(double distance, double time){
         double totalFare = FARE_PER_KM*distance+FARE_PER_MINUTE*time;
-        if (totalFare<5) {
-            totalFare = 5;
-            return totalFare;
+        if (totalFare<MINIMUM_FARE) {
+
+            return MINIMUM_FARE;
         }
         else
         return totalFare;
